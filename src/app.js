c20d89d2d9ca8929f08947e75342d64c2c652d42
Aksh Naik <akshnaik1907@gmail.com>
2026-02-05 15:51:12 +0530

    add user routes .

--- src/app.js
+++ src/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// routes
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.json({ message: "DevHire API is running" });
