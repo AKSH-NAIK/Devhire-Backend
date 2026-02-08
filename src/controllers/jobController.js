cd4b8ee34cc366bf5ee1e484d7a35ebe11ddb7d4
Aksh Naik <akshnaik1907@gmail.com>
2026-02-08 13:57:09 +0530

    Modified CRUD Operations .

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -23,3 +23,41 @@ exports.getJobs = async (req, res) => {
     const jobs = await Job.find();
     res.json({ jobs });
 };
+// Get single job
+exports.getJobById = async (req, res) => {
+    const job = await Job.findById(req.params.id);
+    if (!job) return res.status(404).json({ message: "Job not found" });
+
+    res.json(job);
+};
+
+// Update job
+exports.updateJob = async (req, res) => {
+    const job = await Job.findById(req.params.id);
+    if (!job) return res.status(404).json({ message: "Job not found" });
+
+    if (job.postedBy.toString() !== req.user.id) {
+        return res.status(401).json({ message: "Not authorized" });
+    }
+
+    const updated = await Job.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+    );
+
+    res.json({ message: "Job updated", job: updated });
+};
+
+// Delete job
+exports.deleteJob = async (req, res) => {
+    const job = await Job.findById(req.params.id);
+    if (!job) return res.status(404).json({ message: "Job not found" });
+
+    if (job.postedBy.toString() !== req.user.id) {
+        return res.status(401).json({ message: "Not authorized" });
+    }
+
+    await job.deleteOne();
+    res.json({ message: "Job deleted" });
+};
--- src/routes/jobRoutes.js
+++ src/routes/jobRoutes.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 const auth = require('../middleware/auth');
-const { createJob, getJobs } = require('../controllers/jobController');
+const {
+    createJob,
+    getJobs,
+    getJobById,
+    updateJob,
+    deleteJob
+} = require('../controllers/jobController');
+
 
 // CREATE JOB (protected)
 router.post('/', auth, createJob);
 router.get('/', getJobs);
+router.get('/:id', getJobById);
+router.put('/:id', auth, updateJob);
+router.delete('/:id', auth, deleteJob);
 
 module.exports = router;
