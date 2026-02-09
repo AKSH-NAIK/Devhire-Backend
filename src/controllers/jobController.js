47826f522ac06c7e4cc234cd44948449e9314f8b
Aksh Naik <akshnaik1907@gmail.com>
2026-02-09 15:28:50 +0530

    Added filters ,pagination and search .

--- src/controllers/jobController.js
+++ src/controllers/jobController.js
@@ -61,3 +61,35 @@ exports.deleteJob = async (req, res) => {
     await job.deleteOne();
     res.json({ message: "Job deleted" });
 };
+exports.getJobs = async (req, res) => {
+    const { search, location, page = 1, limit = 5 } = req.query;
+
+    let query = {};
+
+    // Search by title or company
+    if (search) {
+        query.$or = [
+            { title: { $regex: search, $options: "i" } },
+            { company: { $regex: search, $options: "i" } }
+        ];
+    }
+
+    // Filter by location
+    if (location) {
+        query.location = { $regex: location, $options: "i" };
+    }
+
+    const jobs = await Job.find(query)
+        .skip((page - 1) * limit)
+        .limit(Number(limit))
+        .sort({ createdAt: -1 });
+
+    const total = await Job.countDocuments(query);
+
+    res.json({
+        total,
+        page: Number(page),
+        pages: Math.ceil(total / limit),
+        jobs
+    });
+};
