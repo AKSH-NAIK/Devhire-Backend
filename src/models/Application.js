497fab9fb1c1021011f27ec0129963beb13fcb12
Aksh Naik <akshnaik1907@gmail.com>
2026-02-24 15:40:19 +0530

    Adding Form Submission .

--- src/models/Application.js
+++ src/models/Application.js
@@ -1,13 +1,35 @@
 const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema(
-    {
-        job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
-        user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-        resume: { type: String ,required:true},          
-        status: { type: String, default: "applied" }
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Job",
+      required: true,
     },
-    { timestamps: true }
+    candidate: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    coverLetter: {
+      type: String,
+    },
+    resume: {
+      type: String, // file path
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "shortlisted", "rejected"],
+      default: "pending",
+    },
+  },
+  { timestamps: true }
 );
-applicationSchema.index({ job: 1, user: 1 }, { unique: true });
+
 module.exports = mongoose.model("Application", applicationSchema);
