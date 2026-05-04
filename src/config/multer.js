cec65c7d4709338022e3899206c5cee967829540
Aksh Naik <akshnaik1907@gmail.com>
2026-05-04 14:14:15 +0530

    .

--- src/config/multer.js
+++ src/config/multer.js
@@ -30,7 +30,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage,
   limits: {
-    fileSize: 5 * 1024 * 1024 // 5MB
+    fileSize: 5 * 1024 * 1024 
   },
   fileFilter
 });
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -5,20 +5,20 @@ module.exports = async function (req, res, next) {
     try {
         const header = req.headers.authorization;
 
-        // 1️⃣ Check if token exists
+        // 1️ Check if token exists
         if (!header || !header.startsWith('Bearer ')) {
             return res.status(401).json({
                 message: "No token, authorization denied"
             });
         }
 
-        // 2️⃣ Extract token
+        // 2️ Extract token
         const token = header.split(' ')[1];
 
-        // 3️⃣ Verify token
+        // 3 Verify token
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        // 4️⃣ Find user & attach to request
+        // 4️ Find user & attach to request
         const user = await User.findById(decoded.id).select('-password');
 
         if (!user) {
